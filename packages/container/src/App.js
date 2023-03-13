fe08b53b0069dcc0a5175ccb21351a513587f116
samveltadevosyan <stadevosyan@servicetitan.com>
2023-03-13 18:00:29 +0400

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+import MarketingApp from "./components/MarketingApp";
+export default () => {
+    return <div>
+        <h1>hi from container!</h1>
+        <hr />
+        <MarketingApp />
+    </div>
+}
