026c80eec95986454ad9b8d5864004ea422f8458
samveltadevosyan <stadevosyan@servicetitan.com>
2023-03-13 18:56:58 +0400

    remove aws cli stuff

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,6 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
     return <div>
         <h1>hi from container!</h1>
-
         <hr />
         <MarketingApp />
     </div>
